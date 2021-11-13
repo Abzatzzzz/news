f5f636a607ee7a70f782abf12f3a23d395d2c001
abzatzzz <supp.ex.it@gmail.com>
2021-11-13 08:56:35 +0200

    requirements txt added

--- mynews/settings.py
+++ mynews/settings.py
@@ -13,7 +13,7 @@ load_dotenv(os.path.join(project_folder, ".env"))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
