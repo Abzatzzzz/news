49e11fa29c9da59fd64ea652d8d7ca54e44fcb81
abzatzzz <supp.ex.it@gmail.com>
2021-11-14 17:47:20 +0200

    bootstrap added

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . models import News
 
 def index(request):
-    return render(request, "news/index.html")
+    news = News.objects.all()
+    return render(request, 'news/index.html', {'news': news})
