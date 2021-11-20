c3f266b3ba446d3fa6bd18d5a129d3a1d1ca4daf
abzatzzz <supp.ex.it@gmail.com>
2021-11-20 10:48:23 +0200

    params in url requests

--- news/urls.py
+++ news/urls.py
@@ -7,4 +7,5 @@ app_name = "news"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category/<int:category_id>', views.get_category, name="category"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect
-from . models import News
+from . models import News, Category
+
 
 def index(request):
     news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news})
+    categories = Category.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'categories': categories})
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'categories': categories})
