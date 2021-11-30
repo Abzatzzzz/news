b08710a967393370bd02648b15b26095dbe7c711
abzatzzz <supp.ex.it@gmail.com>
2021-11-30 13:33:39 +0200

    add get_absolute_url()

--- news/urls.py
+++ news/urls.py
@@ -8,4 +8,5 @@ app_name = "news"
 urlpatterns = [
     path('', views.index, name="home"),
     path('category/<int:category_id>', views.get_category, name="category"),
+    path('<int:news_id>', views.get_news, name='news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -11,3 +11,6 @@ def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     categories = Category.objects.all()
     return render(request, 'news/category.html', {'news': news, 'categories': categories})
+
+def get_news(request, news_id):
+    return render(request, 'news/news_detail.html', {'news': News.objects.get(pk=news_id)})
